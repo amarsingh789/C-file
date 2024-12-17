1fa501d0c039d2803594c8c89ed609187d1b1746
Amar Singh <amar298976@gmail.com>
2024-12-17 21:08:45 +0530

    Create operator.js
    adding operator

---
+++ Leacture-1/operator.js
@@ -0,0 +1,22 @@
+//this file give information about operatorin JS
+//Arthmetic operator
+let a = 5;
+let b = 10;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a**b);
+console.log(a%b);
+
+//Unary Operator
+let c = 10;
+console.log(c++);//post increment (use, then change)
+console.log(++c);//pre-increment (change, then use)
+
+//Assignmnt operator
+let age = 10;
+age = age+1;//normal way
+age+ = 1; //assignment way
+//same as use (-= , *= , /= , %= and many more)
+
