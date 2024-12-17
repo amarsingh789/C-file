5cbcbc8baef5c4fdc184c5b22d978fce7359251c
Amar Singh <amar298976@gmail.com>
2024-12-17 20:52:17 +0530

    Update and rename c to Leacture-1/app.js
    adding basic info

---
+++ Leacture-1/app.js
@@ -0,0 +1,16 @@
+//Everyone this is javaScript file 
+
+//1)print any thing string and variable use 
+
+//string case
+console.log("Hello world!");
+//variable case
+let name = "Adam";
+console,log(name);
+
+//2)Template Literal
+let a =5;
+let b = 6;
+console.log("total number will be",a+b");//normal use
+//using back tick(``)
+console.log(`total number will be ${a+b}`);//using template literal
