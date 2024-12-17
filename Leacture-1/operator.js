2d5785897f8adaf75a25f7dcf63e26ef24a29542
Amar Singh <amar298976@gmail.com>
2024-12-17 21:41:38 +0530

    Update operator.js
    adding operation data

--- Leacture-1/operator.js
+++ Leacture-1/operator.js
@@ -20,3 +20,34 @@ age = age+1;//normal way
 age+ = 1; //assignment way
 //same as use (-= , *= , /= , %= and many more)
 
+//Comparison Operator
+/*
+>(Greater)
+>=(Greater than)
+<(Less)
+<=(Less than)
+==(Equal to)
+!=(Not equal to)
+*/
+let age = 18;
+console.log(18 > age);
+console.log(18 >= age);
+console.log(18 < age);
+console.log(18 <= age);
+console.log(18 == age);
+console.log(18 != age);
+
+//diffence b/w == & ===
+//== is compare value,not type
+// === is compare type & value
+
+//Comparision for non-number
+//value of small alphabate
+let a = 61;
+let b = 62;
+//so on
+
+//value of capatile letter 
+let A = 41;
+let B = 42;
+//so on
